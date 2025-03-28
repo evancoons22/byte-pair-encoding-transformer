bd79d5f6556b8f39b022413aa464437d2998b4c5
Evan Coons <evancoons22@gmail.com>
2025-03-27 23:53:55 -0700

    attention implemented, build file updated

--- nob.c
+++ nob.c
@@ -3,6 +3,7 @@
 
 #define BUILD_DIR "./build/"
 
+
 int main(int argc, char **argv)
 {
 
@@ -10,34 +11,23 @@ int main(int argc, char **argv)
 
     Nob_Cmd cmd = {0};
 
-    #ifdef _WIN32
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [bpe|train]\n", argv[0]);
+        return 1;
+    }
+    if (strcmp(argv[1], "bpe") == 0) { 
         nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", BUILD_DIR"bpe", "bpe.c");
-        if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
-        nob_cmd_append(&cmd, "./build/bpe.exe");
-        if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
-    #else
-        nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-O3", "-o", BUILD_DIR"bpe", "bpe.c");
-        if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+        (!nob_cmd_run_sync_and_reset(&cmd));
         nob_cmd_append(&cmd, "./build/bpe");
-        if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
-    #endif
-
-
-   // for when compiling the hashmap too
-   //
-   // if (argc < 2) {
-   //     fprintf(stderr, "Usage: %s [bpe|hash]\n", argv[0]);
-   //     return 1;
-   // }
-   // if (strcmp(argv[1], "bpe") == 0) { 
-   // } 
-   // else if (strcmp(argv[1], "hash") == 0) { 
-   //     nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", BUILD_DIR"hash", "hashmap.c");
-   //     if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
-   //     nob_cmd_append(&cmd, "./build/hash");
-   //     if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
-   // } 
-
-
+        if (!nob_cmd_run_sync_and_reset(&cmd)) ;
+    } else if (strcmp(argv[1], "train") == 0) {  
+        nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-lm", "-o", BUILD_DIR"train", "train.c");
+        (!nob_cmd_run_sync_and_reset(&cmd));
+        nob_cmd_append(&cmd, "./build/train");
+        if (!nob_cmd_run_sync_and_reset(&cmd));
+    } else {  
+        fprintf(stderr, "Usage: %s [bpe|train]\n", argv[0]);
+        return 1;
+    } 
     return 0;
 }
--- train.c
+++ train.c
@@ -2,15 +2,13 @@
 #include <stdlib.h> 
 #include <math.h>
 
-#define DIMENSION_EMBEDDING 256
-#define DIMENSION_HIDDEN 256 // same as above?  hidden = embedding? 
-#define DIMENSION_KEYS 64
-#define DIMENSION_VALUES 64
-#define DIMENSION_MODEL (Embedding size?)
-#define NUM_HEADS 4
-#define NN_SIZE 1024
 
-#define LINEAR_1
+#define DIMENSION_EMBEDDING 256
+#define DIMENSION_HIDDEN 256  // usually the same as the embedding (model) dimension
+#define NUM_HEADS 4
+#define DIMENSION_KEYS (DIMENSION_HIDDEN / NUM_HEADS)  // 256 / 4 = 64
+#define DIMENSION_VALUES (DIMENSION_HIDDEN / NUM_HEADS) // 256 / 4 = 64
+#define NN_SIZE 1024  // 4 * DIMENSION_HIDDEN
 
 typedef struct { 
     float* items; 
@@ -23,8 +21,6 @@ typedef struct {
     size_t count;
     size_t capacity;
 } Weights;
-
-
 
 void mha_forward();
 void mmha_forward();
@@ -65,7 +61,96 @@ void softmax(float input[], float output[], size_t size) {
     } 
 } 
 
+void softmax_matrix(float input[], float output[], size_t N, size_t M) {
+    // Apply softmax row-wise (each row sums to 1)
+    for (size_t i = 0; i < N; i++) {
+        // Find max value in the row for numerical stability
+        float max_val = input[i * M];
+        for (size_t j = 1; j < M; j++) {
+            if (input[i * M + j] > max_val) {
+                max_val = input[i * M + j];
+            }
+        }
+        float sum = 0.0f;
+        for (size_t j = 0; j < M; j++) {
+            sum += exp(input[i * M + j] - max_val);
+        }
+        for (size_t j = 0; j < M; j++) {
+            output[i * M + j] = exp(input[i * M + j] - max_val) / sum;
+        }
+    }
+}
+
+void transpose(float X[], int N, int M) {
+    float *temp = malloc(N * M * sizeof(float));
+    for (int i = 0; i < N * M; i++) { temp[i] = X[i]; }
+    // Transpose from temp back to X
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            X[j * N + i] = temp[i * M + j];
+        }
+    }
+    free(temp);
+}
+
+void matmul(float A[], float B[], int N, int D, int M, float result[]) { 
+    // A is N x D, B is D x M, result is N x M
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            result[i * M + j] = 0;
+            for (int k = 0; k < D; k++) {
+                result[i * M + j] += A[i * D + k] * B[k * M + j];
+            }
+        }
+    }
+}
+
+// size is the input size
+// Q, K, V have already been projected here to size x d_k
+void attention(float Q[], float K[], float V[], int size) { 
+    // Q * K^T
+    transpose(K, size, DIMENSION_KEYS);
+    float *QKT = malloc(size * size * sizeof(float));
+    matmul(Q, K, size, DIMENSION_KEYS, size, QKT);
+    // scale
+    float factor = sqrt(DIMENSION_KEYS);
+    for (int i = 0; i < size; i ++ ) { 
+        for (int j = 0; j < size; j ++ ) { 
+            QKT[i * size + j] /= factor;
+        } 
+    } 
+    //softmax
+    float *QKT_out = malloc(size * size * sizeof(float));
+    softmax_matrix(QKT, QKT_out, size, size);
+    float *result = malloc(size * DIMENSION_VALUES * sizeof(float));
+    matmul(QKT_out, V, size, size, DIMENSION_VALUES, result);
+
+
+    free(QKT);
+    free(QKT_out);
+    free(result);
+
+} 
+
 int main() { 
-    printf("test\n");
+    // TODO: load the byte pair encoding from the previous program.
+    // TODO: accept input from the user in the command line and then run the forward part
+    float* pe = malloc(10000 * DIMENSION_EMBEDDING * sizeof(float));
+    compute_positional_encoding(pe, 10000, DIMENSION_EMBEDDING);
+    // TODO: convert pe to matrix
+    
+
+    // dummy 100 token input
+    int size = 100;
+
+    // TODO: learned matrices W_q, W_k, W_v
+    // dummy scaled attention, 
+    float Q[size * DIMENSION_KEYS];
+    float K[size * DIMENSION_KEYS];
+    float V[size * DIMENSION_VALUES];
+    attention(Q, K, V, size);
+
+    printf("trained\n");
+    // TODO: much much more
 
 } 
